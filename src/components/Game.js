4ee323a0f8c4cfd58e69232ccb5d5fdb1e229533
Taranjeet Singh <singhtaran1515@gmail.com>
2020-04-29 14:28:11 +0530

    Completed the game

---
+++ src/components/Game.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Board from './Board'
+
+class Game extends React.Component {
+  render(){
+    return(
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+      </div>
+
+    );
+  }
+}
+ 
+export default Game
+
+// export default Greet;
